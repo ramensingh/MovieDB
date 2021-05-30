a103a08b5ee3652ac48b556afdff0bb675567a02
Raman Singh <64769380+ramensingh@users.noreply.github.com>
2021-05-30 16:31:54 +0530

    Add files via upload

---
+++ MovieDB_/MovieDB/app.py
@@ -0,0 +1,132 @@
+import flask
+import difflib
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField, validators
+import pickle
+import sqlite3
+import os
+import numpy as np
+from vectorizer import vect
+
+app = flask.Flask(__name__, template_folder='templates')
+
+df2 = pd.read_csv('./model/tmdb.csv')
+
+count = CountVectorizer(stop_words='english')
+count_matrix = count.fit_transform(df2['soup'])
+
+cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
+
+df2 = df2.reset_index()
+indices = pd.Series(df2.index, index=df2['title'])
+all_titles = [df2['title'][i] for i in range(len(df2['title']))]
+
+def get_recommendations(title):
+    cosine_sim = cosine_similarity(count_matrix, count_matrix)
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:11]
+    movie_indices = [i[0] for i in sim_scores]
+    tit = df2['title'].iloc[movie_indices]
+    dat = df2['release_date'].iloc[movie_indices]
+    return_df = pd.DataFrame(columns=['Title','Year'])
+    return_df['Title'] = tit
+    return_df['Year'] = dat
+    return return_df
+
+
+@app.route("/")
+def hello():
+    return flask.render_template("home.html")
+
+@app.route('/recc', methods=['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        return(flask.render_template('index.html'))
+            
+    if flask.request.method == 'POST':
+        m_name = flask.request.form['movie_name']
+        m_name = m_name.title()
+#        check = difflib.get_close_matches(m_name,all_titles,cutout=0.50,n=1)
+        if m_name not in all_titles:
+            return(flask.render_template('negative.html',name=m_name))
+        else:
+            result_final = get_recommendations(m_name)
+            names = []
+            dates = []
+            for i in range(len(result_final)):
+                names.append(result_final.iloc[i][0])
+                dates.append(result_final.iloc[i][1])
+
+            return flask.render_template('positive.html',movie_names=names,movie_date=dates,search_name=m_name)
+
+######## Preparing the Classifier
+cur_dir = os.path.dirname(__file__)
+clf = pickle.load(open(os.path.join(cur_dir,
+                 'pkl_objects',
+                 'classifier.pkl'), 'rb'))
+db = os.path.join(cur_dir, 'reviews.sqlite')
+
+def classify(document):
+    label = {0: 'negative', 1: 'positive'}
+    X = vect.transform([document])
+    y = clf.predict(X)[0]
+    proba = np.max(clf.predict_proba(X))
+    return label[y], proba
+
+def train(document, y):
+    X = vect.transform([document])
+    clf.partial_fit(X, [y])
+
+def sqlite_entry(path, document, y):
+    conn = sqlite3.connect(path)
+    c = conn.cursor()
+    c.execute("INSERT INTO review_db (review, sentiment, date)"\
+    " VALUES (?, ?, DATETIME('now'))", (document, y))
+    conn.commit()
+    conn.close()
+
+######## Flask
+class ReviewForm(Form):
+    moviereview = TextAreaField('',
+                                [validators.DataRequired(),
+                                validators.length(min=15)])
+
+@app.route('/classifier')
+def index():
+    form = ReviewForm(request.form)
+    return render_template('reviewform.html', form=form)
+
+@app.route('/results', methods=['POST'])
+def results():
+    form = ReviewForm(request.form)
+    if request.method == 'POST' and form.validate():
+        review = request.form['moviereview']
+        y, proba = classify(review)
+        return render_template('results.html',
+                                content=review,
+                                prediction=y,
+                                probability=round(proba*100, 2))
+    return render_template('reviewform.html', form=form)
+
+@app.route('/thanks', methods=['POST'])
+def feedback():
+    feedback = request.form['feedback_button']
+    review = request.form['review']
+    prediction = request.form['prediction']
+
+    inv_label = {'negative': 0, 'positive': 1}
+    y = inv_label[prediction]
+    if feedback == 'Incorrect':
+        y = int(not(y))
+    train(review, y)
+    sqlite_entry(db, review, y)
+    return render_template('thanks.html')
+
+
+if __name__ == '__main__':
+    app.run()
